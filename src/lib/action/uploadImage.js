57e7b1dc988b95cf986f91b4b1b95e1395934ecb
dulal71 <ahmeddulal4211@gmail.com>
2026-06-26 18:24:31 +0600

    update image issue

--- src/lib/action/uploadImage.js
+++ src/lib/action/uploadImage.js
@@ -1,9 +1,13 @@
 'use server'
 
-export const uploadUserImage = async (formData) => {
+
   const IMGBB_API_KEY = process.env.IMAGE_UPLOAD_API;
+export const uploadUserImage = async (file) => {
 
 
+ const formData = new FormData();
+
+  formData.append("image", file);
   const res = await fetch(
     `https://api.imgbb.com/1/upload?key=${IMGBB_API_KEY}`,
     {
